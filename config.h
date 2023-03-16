209fcc7b668f84786b68a4d143832dd264736938
VoidWalker287 <kjsmith04@outlook.com>
2023-03-16 16:44:13 -0400

    Add header file

---
+++ config.h
@@ -0,0 +1,22 @@
+void examProblem1(void);
+void examProblem2(void);
+void examProblem3(void);
+void examProblem4(void);
+void examProblem5(void);
+void examProblem6(void);
+void examProblem7(void);
+void examProblem8(void);
+void examProblem9(void);
+void examProblem10(void);
+void examProblem11(void);
+void examProblem12(void);
+void examProblem13(void);
+void examProblem14(void);
+void examProblem15(void);
+void examProblem16(void);
+void examProblem17(void);
+void examTrueFalse(void);
+
+void println(void);
+unsigned char start = 1;
+void printsep(void);
